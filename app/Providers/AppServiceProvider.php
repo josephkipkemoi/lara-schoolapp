4f1030e52f55f8769127a542cb662ca75f2a2903
josephkipkemoi <jkemboe@gmail.com>
2022-02-16 16:00:57 -0500

    Avoid accidental mails

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if(! app()->environment('production') )
+        {
+            // To add custom email
+            Mail::alwaysTo('jkemboe@gmail.com');
+        }
     }
 }
