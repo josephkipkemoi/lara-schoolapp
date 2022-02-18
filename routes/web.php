eb9b51268ac809ecace74a23aa09f402b340825f
josephkipkemoi <jkemboe@gmail.com>
2022-02-18 08:12:19 -0500

    Route grouping

--- routes/web.php
+++ routes/web.php
@@ -42,22 +42,31 @@ Route::get('/downloads', [DownloadController::class, '__invoke']);
 // Admin Dashboard Controller
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard'); 
 
 // These routes to be used by admin when adding/removing personnel/staff employed by school
-Route::get('/personnel/create', [PersonnelController::class, 'create'])->name('personnel.create');
-Route::post('/personnel', [PersonnelController::class, 'store'])->name('personnel.store');
-Route::delete('/personnel/{personnel}', [PersonnelController::class, 'destroy'])->name('personnel.destroy');
+Route::controller(PersonnelController::class)->group(function(){
+  Route::get('/personnel/create', 'create')->name('personnel.create');
+  Route::post('/personnel', 'store')->name('personnel.store');
+  Route::delete('/personnel/{personnel}', 'destroy')->name('personnel.destroy');
+});
+
 
 // These routes to be used by admin when adding/removing school events
-Route::get('/events/create', [EventController::class, 'create'])->name('event.create');
-Route::post('/events', [EventController::class, 'store'])->name('event.store');
+Route::controller(EventController::class)->group(function(){
+  Route::get('/events/create', 'create')->name('event.create');
+  Route::post('/events', 'store')->name('event.store');
+});
+
 
 // These routes to be used by admin when adding/removing school reports
-Route::get('reports/create', [ReportController::class, 'create'])->name('report.create');
-Route::post('reports/report', [ReportController::class, 'store'])->name('report.store');
-Route::post('reports', [ReportController::class, 'storereport'])->name('report.storereport');
-Route::delete('reports/{report}/remove', [ReportController::class, 'deletereport'])->name('report.deletereport');
+Route::controller(ReportController::class)->group(function() {
+  Route::get('reports/create', 'create')->name('report.create');
+  Route::post('reports/report', 'store')->name('report.store');
+  Route::post('reports', 'storereport')->name('report.storereport');
+  Route::delete('reports/{report}/remove', 'deletereport')->name('report.deletereport');
+});
+
 
 // Download REPORTS PDF
 Route::get('/report/show/{report}', function($report) {
@@ -74,16 +83,22 @@ Route::get('/downloads/show/{download}', function($download) {
 });
 
 // These routes to be used when uploading file that will be on the download route
-Route::get('downloads/create', [DownloadController::class, 'create'])->name('download.create');
-Route::post('downloads/store', [DownloadController::class, 'store'])->name('download.store');
+Route::controller(DownloadController::class)->group(function(){
+  Route::get('downloads/create', 'create')->name('download.create');
+  Route::post('downloads/store', 'store')->name('download.store');
+});
+
 
 // This route will be used by general users who wish to leave the school management a message
 Route::post('/conctact/store', [ContactController::class, 'store'])->name('contact.store');
 
 // This route will be used by admin when adding photo albums
-Route::get('/photos/create', [AlbumController::class, 'create'])->name('photo.create');
-Route::post('/photos', [AlbumController::class, 'store'])->name('album.store');
-Route::get('/albums/{album}', [AlbumController::class, 'show'])->name('album.show');
+Route::controller(AlbumController::class)->group(function(){
+  Route::get('/photos/create', 'create')->name('photo.create');
+  Route::post('/photos', 'store')->name('album.store');
+  Route::get('/albums/{album}', 'show')->name('album.show');
+});
+
 
 // Route showing contact notifications
 Route::get('/notifications', [NotificationController::class, 'show'])->name('notification.show');
